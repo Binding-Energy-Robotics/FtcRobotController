cd232f33d5147f05a4ce340f65b44b86b07a3ec4
Alex Prichard <70983271+ChillyCoyote273@users.noreply.github.com>
2022-12-12 16:51:34 -0500

    tuned x tracking

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Roadrunner/drive/opmode/StraightTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Roadrunner/drive/opmode/StraightTest.java
@@ -18,7 +18,7 @@ import org.firstinspires.ftc.teamcode.Roadrunner.drive.SampleTankDrive;
 @Config
 @Autonomous(group = "drive")
 public class StraightTest extends LinearOpMode {
-    public static double DISTANCE = 60; // in
+    public static double DISTANCE = 160; // in
 
     @Override
     public void runOpMode() throws InterruptedException {
