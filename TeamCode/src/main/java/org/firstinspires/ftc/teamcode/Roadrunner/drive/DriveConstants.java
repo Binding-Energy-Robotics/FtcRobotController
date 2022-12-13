170075de8632079b4904a241ac13f522a0b96c8d
Alex Prichard <70983271+ChillyCoyote273@users.noreply.github.com>
2022-12-13 14:10:33 -0500

    tuned max velocity

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Roadrunner/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Roadrunner/drive/DriveConstants.java
@@ -31,9 +31,9 @@ public class DriveConstants {
      * If using the built-in motor velocity PID, update MOTOR_VELO_PID with the tuned coefficients
      * from DriveVelocityPIDTuner.
      */
-    public static final boolean RUN_USING_ENCODER = false;
-    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(0, 0, 0,
-            getMotorVelocityF(MAX_RPM / 60 * TICKS_PER_REV));
+    public static final boolean RUN_USING_ENCODER = false; // tuned by Alex Prichard on 13 Dec 2022
+    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(0, 0, 0, 13.276
+            /*getMotorVelocityF(MAX_RPM / 60 * TICKS_PER_REV)*/);
 
     /*
      * These are physical constants that can be determined from your robot (including the track
@@ -65,9 +65,9 @@ public class DriveConstants {
      * small and gradually increase them later after everything is working. All distance units are
      * inches.
      */
-    public static double MAX_VEL = 45;
+    public static double MAX_VEL = 45; // tuned by Alex Prichard on 13 Dec 2022
     public static double MAX_ACCEL = 30;
-    public static double MAX_ANG_VEL = 2.79; // tuned by Alex Prichard on 12 Dec 2022
+    public static double MAX_ANG_VEL = 2.5; // tuned by Alex Prichard on 12 Dec 2022
     public static double MAX_ANG_ACCEL = Math.toRadians(60);
 
 
