ceeebd38062661cd633788a72623a07626d607a5
Alex Prichard <70983271+ChillyCoyote273@users.noreply.github.com>
2023-01-20 21:22:00 -0500

    competition code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Competition/LeftAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Competition/LeftAuto.java
@@ -37,16 +37,16 @@ public class LeftAuto extends CommandOpMode {
 	public static final Pose2d START_POSE_A = new Pose2d(-34, -48, Math.toRadians(120));
 	public static final Pose2d START_POSE_B = new Pose2d(-36, -24, Math.toRadians(70));
 
-	public static final Pose2d SCORE_POSE_ZERO = new Pose2d(-28, -8.5, Math.toRadians(45));
-	public static final Pose2d SCORE_POSE_ONE = new Pose2d(-27, -9.5, Math.toRadians(45));
-	public static final Pose2d SCORE_POSE_TWO = new Pose2d(-26, -10.5, Math.toRadians(45));
-	public static final Pose2d SCORE_POSE_THREE = new Pose2d(-25.5, -9.5, Math.toRadians(45));
+	public static final Pose2d SCORE_POSE_ZERO = new Pose2d(-28, -8.2, Math.toRadians(45));
+	public static final Pose2d SCORE_POSE_ONE = new Pose2d(-27, -9.2, Math.toRadians(45));
+	public static final Pose2d SCORE_POSE_TWO = new Pose2d(-26, -10.2, Math.toRadians(45));
+	public static final Pose2d SCORE_POSE_THREE = new Pose2d(-25, -11.2, Math.toRadians(45));
 	public static final Pose2d SCORE_POSE_FOUR = new Pose2d(-25, -10, Math.toRadians(45));
 	public static final Pose2d SCORE_POSE_FIVE = new Pose2d(-25, -10.5, Math.toRadians(45));
 
 	public static final Pose2d CONE_POSE_ONE = new Pose2d(-58, -12.8, Math.toRadians(0));
-	public static final Pose2d CONE_POSE_TWO = new Pose2d(-58, -12.8, Math.toRadians(0));
-	public static final Pose2d CONE_POSE_THREE = new Pose2d(-55.5, -15, Math.toRadians(0));
+	public static final Pose2d CONE_POSE_TWO = new Pose2d(-57.75, -12.8, Math.toRadians(0));
+	public static final Pose2d CONE_POSE_THREE = new Pose2d(-57.5, -12.8, Math.toRadians(0));
 	public static final Pose2d CONE_POSE_FOUR = new Pose2d(-55.5, -15, Math.toRadians(0));
 	public static final Pose2d CONE_POSE_FIVE = new Pose2d(-55.5, -15.5, Math.toRadians(0));
 
@@ -107,7 +107,7 @@ public class LeftAuto extends CommandOpMode {
 		flipper = new Flipper(hardwareMap, telemetry);
 		claw = new Claw(hardwareMap);
 		claw.open();
-//		camera = new Camera(hardwareMap);
+		camera = new Camera(hardwareMap);
 
 
 		Trajectory junctionToConesOne = drive.trajectoryBuilder(SCORE_POSE_ZERO, true)
@@ -201,7 +201,7 @@ public class LeftAuto extends CommandOpMode {
 						new ParallelCommandGroup(
 								park,
 								new SequentialCommandGroup(
-										new AsyncDelay(0.67),
+										new AsyncDelay(0.33),
 										new FlipIn(flipper)
 								)
 						)
@@ -213,7 +213,7 @@ public class LeftAuto extends CommandOpMode {
 				setUpScoring,
 				cycle(5, junctionToConesOne, conesToJunctionOne),
 				cycle(4, junctionToConesTwo, conesToJunctionTwo),
-//				cycle(3, junctionToConesThree, conesToJunctionThree),
+				cycle(3, junctionToConesThree, conesToJunctionThree),
 //				cycle(2, junctionToConesFour, conesToJunctionFour),
 //				cycle(1, junctionToConesFive, conesToJunctionFive),
 				dropForPark,
@@ -226,12 +226,12 @@ public class LeftAuto extends CommandOpMode {
 		telemetry.update();
 		waitForStart();
 		while (!isStarted()) {
-//			camera.periodic();
-//			telemetry.addData("Camera", camera.getConfidences());
+			camera.periodic();
+			telemetry.addData("Camera", camera.getConfidences());
 		}
 
-//		signalSide = camera.getSide();
-//		camera.stop();
+		signalSide = camera.getSide();
+		camera.stop();
 
 		schedule(runAuto);
 	}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Framework/Utilities/AutoEndPose.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Framework/Utilities/AutoEndPose.java
@@ -3,17 +3,20 @@ package org.firstinspires.ftc.teamcode.Framework.Utilities;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 
 public class AutoEndPose {
-	private static Pose2d pose = null;
+	private static Pose2d pose = new Pose2d(0, 0, Math.toRadians(90));
+	private static int slidePosition;
 
-	public static Pose2d getPose() {
-		if (AutoEndPose.pose != null)
-			return AutoEndPose.pose;
-		return new Pose2d(0, 0, Math.toRadians(90));
-	}
+	public static Pose2d getPose() { return AutoEndPose.pose; }
 
-	public static void setPose(Pose2d pose) {
-		AutoEndPose.pose = pose;
-	}
+	public static void setPose(Pose2d pose) { AutoEndPose.pose = pose; }
 
 	public static void clearPose() { setPose(new Pose2d(0, 0, Math.toRadians(90))); }
+
+	public static int getSlidePosition() { return AutoEndPose.slidePosition; }
+
+	public static void setSlidePosition(int slidePosition) {
+		AutoEndPose.slidePosition = slidePosition;
+	}
+
+	public static void clearSlidePosition() { setSlidePosition(0); }
 }
